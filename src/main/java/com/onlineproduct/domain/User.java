3e010655e730fbe7109f89dfb432817947f4de3f
Ramesh KC <kcramesh8@gmail.com>
2017-02-06 00:08:57 -0600

    Creating User model class.

--- src/main/java/com/onlineproduct/domain/User.java
+++ src/main/java/com/onlineproduct/domain/User.java
@@ -1,5 +1,70 @@
 package com.onlineproduct.domain;
 
-public class User {
+import java.util.Date;
 
+public class User {
+	private String username;
+	private String password;
+	private String name;
+	private String email;
+	private int contact;
+	private String address;
+	private Date dateOfBirth;
+	
+	public String getUsername() {
+		return username;
+	}
+	public User(String username, String password, 
+			String name, String email, int contact, 
+			String address,
+			Date dateOfBirth) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.name = name;
+		this.email = email;
+		this.contact = contact;
+		this.address = address;
+		this.dateOfBirth = dateOfBirth;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getContact() {
+		return contact;
+	}
+	public void setContact(int contact) {
+		this.contact = contact;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}	
 }
