c027f589444665c235a4390a1a21ff0bdd192469
Ramesh KC <kcramesh8@gmail.com>
2017-02-06 10:40:31 -0600

    adding UserRepository class.

--- src/main/java/com/onlineproduct/repository/UserRepository.java
+++ src/main/java/com/onlineproduct/repository/UserRepository.java
@@ -1,5 +1,12 @@
 package com.onlineproduct.repository;
 
-public interface UserRepository {
+import java.util.List;
 
+import com.onlineproduct.domain.User;
+
+public interface UserRepository {
+	
+	public void saveOrUpdate(User user);
+	
+	public List<User> userList();
 }
